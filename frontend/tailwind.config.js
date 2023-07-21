9576c8ee10196e99fbe5bb44f84c5c8f0e480abb
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-07-20 23:54:59 -0300

    added theme config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      gray: "hsl(0, 0%, 98%)",
+      "gray-very-light": "hsl(236, 33%, 92%)",
+      "gray-light": "hsl(233, 11%, 84%)",
+      "gray-dark": "hsl(236, 9%, 61%)",
+      "gray-very-dark": "hsl(235, 19%, 35%)",
+      "blue-very-dark": "hsl(235, 21%, 11%)",
+      "blue-very-dark-desaturated": "hsl(235, 24%, 19%)",
+      "blue-light": "hsl(234, 39%, 85%)",
+      "blue-light-hover": "hsl(236, 33%, 92%)",
+      "blue-grayish-dark": "hsl(234, 11%, 52%)",
+      "blue-very-dark-grayish": "hsl(233, 14%, 35%)",
+      "blue-very-dark-grayish-2": "hsl(237, 14%, 26%)",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
