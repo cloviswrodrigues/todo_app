4ce306caccaf3a0a72e30e7b70440b6f4d864ab7
cloviswrodrigues@gmail.com <cloviswrodrigues@gmail.com>
2023-07-26 23:59:31 -0300

    fix todo list

--- frontend/src/components/TodoItem/index.tsx
+++ frontend/src/components/TodoItem/index.tsx
@@ -7,7 +7,7 @@ type TodoItemProps = {
 const TodoItem = ({ children }: TodoItemProps) => {
     return (
         <li className="flex items-center p-4 pl-6 border-b border-gray-very-light">
-            <button className="border-[1px] block w-5 h-5 rounded-full border-gray-light mr-3"></button>
+            <button className="border block w-5 h-5 rounded-full border-gray-light mr-3"></button>
             <span className="flex-1">{children}</span>
             <div className="w-6"><CloseSVG /></div>
         </li>
--- frontend/src/components/TodoList/index.tsx
+++ frontend/src/components/TodoList/index.tsx
@@ -3,26 +3,33 @@ import TodoItem from "../TodoItem";
 const TodoList = () => {
   return (
     <>
-      <div className="bg-white w-full px-3 pl-4 rounded-md text-sm mb-4 shadow-sm flex items-center">
-        <span className="border-[1px] block w-6 h-5 rounded-full border-gray-light mr-3"></span>
+      <div className="bg-white w-full px-3 pl-6 rounded-md text-sm mb-4 shadow-sm flex items-center">
+        <span className="border block w-5 h-5 rounded-full border-gray-light mr-3"></span>
         <input
-          className="w-full text- py-4"
+          className="py-4"
           type="text"
           placeholder="Create a new todo..."
         />
       </div>
-      <ul className="bg-white rounded-md mb-4 shadow-lg">
-        <TodoItem>Jog around the park 3x</TodoItem>
-        <TodoItem>Jog around the park 3x</TodoItem>
-        <TodoItem>Jog around the park 3x</TodoItem>
-        <TodoItem>Jog around the park 3x</TodoItem>
-        <TodoItem>Jog around the park 3x</TodoItem>
-      </ul>
-      <div className="bg-white p-4 rounded-md flex justify-center gap-4 shadow-sm">
+      <div className="bg-white rounded-md mb-4 shadow-lg">
+        <ul>
+          <TodoItem>Jog around the park 3x</TodoItem>
+          <TodoItem>Jog around the park 3x</TodoItem>
+          <TodoItem>Jog around the park 3x</TodoItem>
+          <TodoItem>Jog around the park 3x</TodoItem>
+          <TodoItem>Jog around the park 3x</TodoItem>
+        </ul>
+        <div className="flex justify-between items-center p-4 pl-6 text-xs text-gray-dark">
+          <p>5 items left</p>
+          <button>Clear Completed</button>
+        </div>
+      </div>
+      <div className="bg-white p-4 rounded-md flex justify-center gap-4 shadow-sm mb-10 text-gray-dark text-sm font-bold">
         <button>All</button>
         <button>Active</button>
         <button>Completed</button>
       </div>
+      <p className="text-sm text-center text-gray-dark">Drag and drop to reorder list</p>
     </>
   );
 };
