0082ceeba993e7d9e83d19e0e3b47166712366a5
cloviswrodrigues@gmail.com <cloviswrodrigues@gmail.com>
2023-07-27 23:25:33 -0300

    fix style TodoItem

--- frontend/src/components/TodoItem/index.tsx
+++ frontend/src/components/TodoItem/index.tsx
@@ -8,8 +8,8 @@ const TodoItem = ({ children }: TodoItemProps) => {
     return (
         <li className="flex items-center p-4 pl-6 border-b border-gray-very-light">
             <button className="border block w-5 h-5 rounded-full border-gray-light mr-3"></button>
-            <span className="flex-1">{children}</span>
-            <div className="w-6"><CloseSVG /></div>
+            <span className="flex-1 text-sm text-gray-very-dark">{children}</span>
+            <div className="w-3"><CloseSVG /></div>
         </li>
     )
 }
