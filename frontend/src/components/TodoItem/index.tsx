6d49c59eb5dce20b455f1a0baa620a49c3086b89
cloviswrodrigues@gmail.com <cloviswrodrigues@gmail.com>
2023-07-31 00:25:57 -0300

    added border gradient in hover

--- frontend/src/components/TodoItem/index.tsx
+++ frontend/src/components/TodoItem/index.tsx
@@ -7,7 +7,7 @@ type TodoItemProps = {
 const TodoItem = ({ children }: TodoItemProps) => {
     return (
         <li className="flex items-center p-4 pl-6 border-b border-gray-very-light md:p-5">
-            <button className="border block w-5 h-5 rounded-full border-gray-light mr-4"></button>
+            <button className="border block w-5 h-5 rounded-full mr-4 border-gray-light border-custom-gradient"></button>
             <span className="flex-1 text-sm text-gray-very-dark md:text-base">{children}</span>
             <div className="w-3"><CloseSVG /></div>
         </li>
