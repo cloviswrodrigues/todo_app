8894c20d64c793ca3ba7fcf63e5af325f281a8d8
cloviswrodrigues@gmail.com <cloviswrodrigues@gmail.com>
2023-07-31 23:18:53 -0300

    adjusted header base height

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as MoonSVG } from "../../assets/icon-moon.svg";
 
 const Header = () => {
   return (
-    <header className="bg-header-mobile bg-no-repeat bg-center bg-cover h-full max-h-[200px] px-5 pt-10 sm:bg-header-desktop md:max-h-[300px] md:pt-16">
+    <header className="bg-header-mobile bg-no-repeat bg-center bg-cover h-full max-h-[200px] px-5 pt-10 basis-[300px] sm:bg-header-desktop md:max-h-[300px] md:pt-16">
       <div className="flex justify-between items-center max-w-lg w-full m-auto">
         <h1 className="text-white text-2xl font-bold tracking-widest md:text-4xl">TODO</h1>
         <span className="w-5 md:w-6">
