7e6c6bdc98630521946c5e416f257f6da9623aa2
cloviswrodrigues@gmail.com <cloviswrodrigues@gmail.com>
2023-09-04 23:59:14 -0300

    adjusted color footer in dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
           <TodoList />
         </div>
       </main>
-      <footer className="bg-gray">
+      <footer className="bg-gray dark:bg-blue-very-dark dark:border-color-bg-blue-very-dark">
         <div className="text-center text-xs text-gray-dark">
           Challenge by <a className="underline" href="https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW" target="_blank">Frontend Mentor</a>. 
           Coded by <a className="underline" href="https://github.com/cloviswrodrigues" target="_blank">Clóvis Rodrigues</a>.
